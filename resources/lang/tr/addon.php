ee07e658bd298750490ac05fae0b000ae70418d5
Metehan Celik <mthncelik@gmail.com>
2022-04-07 12:22:05 +0300

    admin panel translations

---
+++ resources/lang/tr/addon.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'       => 'Throttle Security Check',
+    'name'        => 'Throttle Security Check Extension',
+    'description' => 'Kullanıcılar modülü için oturum açma girişimlerini kısıtlayan bir güvenlik uzantısı.',
+];
